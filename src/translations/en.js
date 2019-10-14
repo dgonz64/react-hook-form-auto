f39194d5fe49813e3dfc3e70b378ed162e4b9f7d
David González <braben@gmail.com>
2019-10-14 20:53:20 +0100

    Silly language update

--- src/translations/en.js
+++ src/translations/en.js
@@ -4,8 +4,8 @@ export const en = {
 	error: {
 		_: 'Error',
 		type: 'Incorrect value, expecting a __type__',
-		minLength: 'Value should be more than __minLength__ characters long',
-		maxLength: 'Value should be less than __maxLength__ characters long',
+		min: 'Value should be more than __minLength__ characters long',
+		max: 'Value should be less than __maxLength__ characters long',
 		required: 'Required',
 		minChildren: 'Expected to have at least __minChildren__',
 		maxChildren: 'Can only have __maxChildren__',
