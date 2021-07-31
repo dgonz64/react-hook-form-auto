59776bc11ce649b53b7bb2e1d1ac8e3d3e7af358
David González <braben@gmail.com>
2021-07-31 09:46:33 +0100

    onChange tests

--- test/schema-onChange.react.test.js
+++ test/schema-onChange.react.test.js
@@ -6,10 +6,12 @@ import { act } from 'react-dom/test-utils'
 import {
   createSchema,
   Autoform,
-  setLanguageByName
+  setLanguageByName,
+  Button
 } from '../src/index'
 import {
   changeInput,
+  changeSelect,
   changeCheckbox
 } from './utils/changeField'
 
@@ -33,6 +35,7 @@ const pet = createSchema('pet', {
     type: 'select',
     options: ['blue', 'yellow'],
     onChange: (value, { name, setHelperText }) => {
+      debugger
       setHelperText(name, `Better not choose ${value}`)
     }
   },
@@ -69,36 +72,81 @@ const owner = createSchema('owner', {
   }
 })
 
-test('Hides according to initiallyVisible', async () => {
+function timedPromise(time = 100) {
+  return new Promise(resolve => {
+    setTimeout(resolve, time)
+  })
+}
+
+test('onChange: hide according to initiallyVisible and change visibility', async () => {
   const app = mount(
     <Autoform schema={owner} />
   )
 
-  const usesHat = app.find('input[name="name"]')
+  const usesHat = app.find('input[name="usesHat"]')
   expect(usesHat).toHaveLength(1)
 
-  const color = app.find('input[name="hatColor"]')
+  const color = app.find('select[name="hatColor"]')
   expect(color).toHaveLength(0)
 
-  // await act(async () => {
-  //   await changeInput(usesHat, true)
-  // })
+  await changeCheckbox(usesHat, true)
+  await app.update()
 
-  // const nowColor = app.find('input[name="hatColor"]')
-  // expect(nowColor).toHaveLength(1)
+  const nowColor = app.find('select[name="hatColor"]')
+  expect(nowColor).toHaveLength(1)
 })
 
-// test('Hides things according to schema\'s initiallyVisible', () => {
-//   const app = mount(
-//     <Autoform schema={owner} />
-//   )
+test('onChange: Change arbitrary input value', async () => {
+  const app = mount(
+    <Autoform schema={owner} />
+  )
 
-//   const name = app.find('input[name="name"]')
-//   expect(name).toHaveLength(1)
+  const name = app.find('input[name="name"]')
+  expect(name).toHaveLength(1)
 
-//   const color = app.find('input[name="hatColor"]')
-//   expect(color).toHaveLength(0)
+  const height = app.find('input[name="height"][value="tall"]')
+  expect(height).toHaveLength(1)
 
-//   const pet0Heads = app.find('input[name="pets.0.heads"]')
-//   expect(pet0Heads).toHaveLength(1)
-// })
+  await changeInput(height, 'tall')
+  await app.update()
+
+  expect(name.instance().value).toBe('André the Giant')
+})
+
+test('onChange: Add and remove array elements', async () => {
+  const app = mount(
+    <Autoform schema={owner} />
+  )
+
+  const name = app.find('input[name="pets.0.heads"]')
+  expect(name).toHaveLength(1)
+
+  const buttons = app.find(Button)
+  expect(buttons).toHaveLength(2)
+
+  await changeInput(name, '42')
+  await app.update()
+
+  const addedButtons = app.find(Button)
+  expect(addedButtons).toHaveLength(3)
+
+  await changeInput(name, '13')
+  await app.update()
+
+  const removedButtons = app.find(Button)
+  expect(removedButtons).toHaveLength(2)
+})
+
+test('onChange: Change helper text', async () => {
+  const app = mount(
+    <Autoform schema={owner} />
+  )
+
+  const hair = app.find('select[name="pets.0.hair"]')
+  expect(hair).toHaveLength(1)
+
+  await changeSelect(hair, 'blue')
+  await app.update()
+
+  expect(app.text()).toMatch('Better not choose blue')
+})
